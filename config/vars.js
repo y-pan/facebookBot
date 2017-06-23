1bc727aa88e3a56e64a5b8bf933c81e6fb09ad34
Yun Kui Pan <panyunkui@gmail.com>
2017-06-23 15:02:47 -0400

    play again

---
+++ config/vars.js
@@ -0,0 +1,36 @@
+module.exports.offerMenus = {
+    offerMenu1:"offerMenu1",
+    offerMenu1_1:"offerMenu1_1",
+    offerMenu1_2:"offerMenu1_2",
+    offerMenu1_3:"offerMenu1_3",
+    
+    offerMenu2:"offerMenu2",
+    offerMenu2_1:"offerMenu2_1",
+    offerMenu2_2:"offerMenu2_2",
+    offerMenu2_3:"offerMenu2_3",
+
+    offerMenu3:"offerMenu3",
+    offerMenu3_1:"offerMenu3_1",
+    offerMenu3_2:"offerMenu3_2",
+    offerMenu3_3:"offerMenu3_3",
+    
+    offerMenu4:"offerMenu4",
+    offerMenu4_1:"offerMenu4_1",
+    offerMenu4_2:"offerMenu4_2",
+    offerMenu4_3:"offerMenu4_3",
+
+    offerMenu5:"offerMenu5",
+    offerMenu5_1:"offerMenu5_1",
+    offerMenu5_2:"offerMenu5_2",
+    offerMenu5_3:"offerMenu5_3",
+
+    offerMenu6:"offerMenu6",
+    offerMenu6_1:"offerMenu6_1",
+    offerMenu6_2:"offerMenu6_2",
+    offerMenu6_3:"offerMenu6_3",
+
+    offerMenu7:"offerMenu7",
+    offerMenu7_1:"offerMenu7_1",
+    offerMenu7_2:"offerMenu7_2",
+    offerMenu7_3:"offerMenu7_3",
+}
--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 'use strict'
 
-const secret = require('./config/secret');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+
+// my Vars and functions
+const Secret = require('./config/secret');          // Secret Vars
+const Vars = require('./config/vars');        // Vars Vars
+const Lib = require('./lib/lib');                   // javascript Library, to keep index.js neat and nice
+
+
 
 const app = express();
 
@@ -23,7 +29,7 @@ app.get('/', function(req, res){
 
 // Facebook
 app.get('/webhook/', function(req, res){
-    if(req.query['hub.verify_token'] === secret.verify_token){
+    if(req.query['hub.verify_token'] === Secret.verify_token){
         res.send(req.query['hub.challenge']); // good
     }
     res.send("Wrong token");
@@ -35,42 +41,35 @@ app.post('/webhook/', (req, res)=>{
     for(let i=0; i< messaging_events.length; i++){
         let event = messaging_events[i];
         let sender = event.sender.id;
+
+        // ____________________>>>>>>>>>
         if(event.message && event.message.text){
             let text = event.message.text;
 
             // logic apply here for what is the sender request, and what to response
+            Lib.decideMessage(sender, text);
+            /*
             if(text.includes("offer")) {
-                sendText(sender, "Here are the offer available near your location: " + "\n   1. Go on a TTC bus and sing a song loud enough!" + "\n   2. Go to the Centennial library and borrow 100 books and read every single words in 1 hour!");
+                Lib.sendText(sender, "Here are the offer available near your location: " + "\n   1. Go on a TTC bus and sing a song loud enough!" + "\n   2. Go to the Centennial Library and borrow 100 books and read every single words in 1 hour!");
                 
             }else{
-                sendText(sender, "What do you mean by: " + text.substring(0, 100) + "?");
-            }
-            
+                Lib.sendText(sender, "What do you mean by: " + text.substring(0, 100) + "?");
+            }*/
+        }
+        // ____________________>>>>>>>>>
+        if(event.postback){
+            let text = JSON.stringify(event.postback);
+            Lib.decideMessage(sender, text);
+            continue;
         }
     }
     res.sendStatus(200);
 });
 
 
-// functions
-function sendText(sender, text){
-    let messageData = {text: text}
-    request({
-        url:"https://graph.facebook.com/v2.6/me/messages",
-        qs:{access_token : secret.access_token},
-        method:"POST",
-        json: {
-            recipient: {id: sender},
-            message: messageData
-        }
-    }, (error, response, body)=>{
-        if(error){
-            console.log("sending error")
-        }else if(response.body.error){
-            console.log("response body error");
-        }
-    })
-}
+
+
+
 
 
 app.listen(app.get('port'), ()=>{
