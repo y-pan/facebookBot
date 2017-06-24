75f47fc13afa853891c54335b3e78ae4f5bfd45c
Yun Kui Pan <panyunkui@gmail.com>
2017-06-24 16:25:12 -0400

    fixing callback

--- config/vars.js
+++ config/vars.js
@@ -1,28 +1,28 @@
-module.exports.buttons= [
+module.exports.collections= [
     { 
         triggerPayload:0,
-        menus:[
+        data:[
             { type:"postback", title:"0->menu1", payload:1 },
             { type:"postback", title:"0->menu2", payload:2 },
             { type:"postback", title:"0->menu3", payload:3 }]
     }
     ,{ 
         triggerPayload:1,
-        menus:[
+        data:[
             { type:"postback", title:"1->menu4", payload:4 },
             { type:"postback", title:"1->menu5", payload:5 },
             { type:"postback", title:"1->menu6", payload:6 }]
     }
     ,{ 
         triggerPayload:2,
-        menus:[
+        data:[
             { type:"postback", title:"2->menu7", payload:7 },
             { type:"postback", title:"2->menu8", payload:8 },
             { type:"postback", title:"2->menu9", payload:9 }]
     }
     ,{ 
         triggerPayload:3,
-        menus:[
+        data:[
             { type:"postback", title:"3->menu10", payload:10 },
             { type:"postback", title:"3->menu11", payload:11 },
             { type:"postback", title:"3->menu12", payload:12 }]
