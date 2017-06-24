57d99c57156aff877d6041e6e67c91dda4a6bbcf
Yun Kui Pan <panyunkui@gmail.com>
2017-06-23 23:54:14 -0400

    trying button menu

--- config/vars.js
+++ config/vars.js
@@ -34,3 +34,91 @@ module.exports.offerMenus= {
     offerMenu7_2:"offerMenu7_2",
     offerMenu7_3:"offerMenu7_3",
 }
+module.exports.level0 = {
+    level:0,
+    menus:[
+        { title:"menu1", payload:1 },
+        { title:"menu2", payload:2 },
+        { title:"menu3", payload:3 },
+        { title:"menu4", payload:4 },
+        { title:"menu5", payload:5 },
+        { title:"menu6", payload:6 },
+    ]
+}
+module.exports.level1 = {
+    level: 1,
+    menus:[
+        { title:"menu1-1", payload:11 },
+        { title:"menu1-2", payload:12 },
+        { title:"menu1-3", payload:13 },
+        { title:"menu1-4", payload:14 },
+        { title:"menu1-5", payload:15 },
+        { title:"menu1-6", payload:16 },
+    ]
+}
+module.exports.level2 = {
+    level: 2,
+    menus:[
+        { title:"menu2-1", payload:21 },
+        { title:"menu2-2", payload:22 },
+        { title:"menu2-3", payload:23 },
+        { title:"menu2-4", payload:24 },
+        { title:"menu2-5", payload:25 },
+        { title:"menu2-6", payload:26 },
+    ]
+}
+module.exports.level3 = {
+    level: 3,
+    menus:[
+        { title:"menu3-1", payload:31 },
+        { title:"menu3-2", payload:32 },
+        { title:"menu3-3", payload:33 },
+        { title:"menu3-4", payload:34 },
+        { title:"menu3-5", payload:35 },
+        { title:"menu3-6", payload:36 },
+    ]
+}
+module.exports.level4 = {
+    level: 4,
+    menus:[
+        { title:"menu4-1", payload:41 },
+        { title:"menu4-2", payload:42 },
+        { title:"menu4-3", payload:43 },
+        { title:"menu4-4", payload:44 },
+        { title:"menu4-5", payload:45 },
+        { title:"menu4-6", payload:46 },
+    ]
+}
+module.exports.level5 = {
+    level: 5,
+    menus:[
+        { title:"menu5-1", payload:51 },
+        { title:"menu5-2", payload:52 },
+        { title:"menu5-3", payload:53 },
+        { title:"menu5-4", payload:54 },
+        { title:"menu5-5", payload:55 },
+        { title:"menu5-6", payload:56 },
+    ]
+}
+module.exports.level6 = {
+    level: 6,
+    menus:[
+        { title:"menu6-1", payload:61 },
+        { title:"menu6-2", payload:62 },
+        { title:"menu6-3", payload:63 },
+        { title:"menu6-4", payload:64 },
+        { title:"menu6-5", payload:65 },
+        { title:"menu6-6", payload:66 },
+    ]
+}
+module.exports.level7 = {
+    level: 7,
+    menus:[
+        { title:"menu7-1", payload:71 },
+        { title:"menu7-2", payload:72 },
+        { title:"menu7-3", payload:73 },
+        { title:"menu7-4", payload:74 },
+        { title:"menu7-5", payload:75 },
+        { title:"menu7-6", payload:76 },
+    ]
+}
