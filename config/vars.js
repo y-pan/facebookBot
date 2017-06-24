51d6dfce67197c0300039a5f5c232db149919e5f
Yun Kui Pan <panyunkui@gmail.com>
2017-06-24 17:15:05 -0400

    debugging

--- config/vars.js
+++ config/vars.js
@@ -1,27 +1,27 @@
 module.exports.collections= [
     { 
-        triggerPayload:0,
+        triggerPayload:'0',
         data:[
             { type:"postback", title:"0->menu1", payload:1 },
             { type:"postback", title:"0->menu2", payload:2 },
             { type:"postback", title:"0->menu3", payload:3 }]
     }
     ,{ 
-        triggerPayload:1,
+        triggerPayload:'1',
         data:[
             { type:"postback", title:"1->menu4", payload:4 },
             { type:"postback", title:"1->menu5", payload:5 },
             { type:"postback", title:"1->menu6", payload:6 }]
     }
     ,{ 
-        triggerPayload:2,
+        triggerPayload:'2',
         data:[
             { type:"postback", title:"2->menu7", payload:7 },
             { type:"postback", title:"2->menu8", payload:8 },
             { type:"postback", title:"2->menu9", payload:9 }]
     }
     ,{ 
-        triggerPayload:3,
+        triggerPayload:'3',
         data:[
             { type:"postback", title:"3->menu10", payload:10 },
             { type:"postback", title:"3->menu11", payload:11 },
