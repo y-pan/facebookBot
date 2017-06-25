7d8dc9e7c1f9fc70ece7f1d06941d887a1ed315e
Yun Kui Pan <panyunkui@gmail.com>
2017-06-25 00:49:56 -0400

    "

--- config/secret.js
+++ config/secret.js
@@ -3,3 +3,7 @@ module.exports = {
     ,access_token:'EAAbvwR6d39IBALEjKwWKktsmCnMySYE2T2xJUUNU2gVxItuJoYLwNQbCQKIhwF4ZAeg7BsJYSUAtBXXxQ1VwtdAPDetvyC6WrCxtLb6simlSiWWIK6WfgteqVnihwssDQRFSpPz9NzdOst6twbsFS5gvtcmhsz8fOhb1f8AZDZD'
     ,requestUri:'https://graph.facebook.com/v2.6/me/messages'
 }
+
+//curl -X POST "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=EAAbvwR6d39IBALEjKwWKktsmCnMySYE2T2xJUUNU2gVxItuJoYLwNQbCQKIhwF4ZAeg7BsJYSUAtBXXxQ1VwtdAPDetvyC6WrCxtLb6simlSiWWIK6WfgteqVnihwssDQRFSpPz9NzdOst6twbsFS5gvtcmhsz8fOhb1f8AZDZD"
+
+//curl -ik -X POST "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=<token>"
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+'use strict'
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
@@ -6,7 +6,9 @@ const request = require('request');
 // my vars and functions
 const secret = require('./config/secret');          // secret vars
 const vars = require('./config/vars');        // vars vars
-const lib = require('./lib/lib1');               // function lib
+//const lib = require('./lib/lib1');               // function lib
+let selfSender;
+let selfTemp=null;
 
 const app = express();
 app.set('port', (process.env.PORT || 5000));
@@ -34,7 +36,7 @@ app.get('/webhook/', function(req, res){
 
 // Here bot reply message to sender on facebook/messager
 app.post('/webhook/', (req, res)=>{
-event.message
+
     req.body.entry[0].messaging.forEach(function(event){
         let sender = event.sender.id;
         //console.log(pageID +"(p)=?(e)" +sender);
@@ -42,62 +44,15 @@ event.message
         console.log("===2.event.message===",JSON.stringify(event.message));
         console.log("===3.event.message.text===",JSON.stringify(event.message.text));
         if(event.message && event.message.text){
-            lib.decideMessage(sender, event.message.text, false);
+            decideMessage(sender, event.message.text, false);
         }
 
         if(event.postback){
             let text = JSON.stringify(event.postback.payload);
-            lib.decideMessage(sender, text, true);
+            decideMessage(sender, text, true);
         }
     })
 
-/*
-    let data = req.body;
-
-    if(data.object === 'page'){
-
-        //data.entry.forEach((entry)=>{
-        //    let pageID = entry.id;
-        //    let timeOfEvent = entry.time;
-
-        //    entry.message.forEach((event)=>{
-            console.log("######req.body######",JSON.stringify(req.body));
-            data.entry[0].messaging.forEach((event)=>{
-                let sender = event.sender.id;
-                //console.log(pageID +"(p)=?(e)" +sender);
-                console.log("===1.event===",JSON.stringify(event));
-                console.log("===2.event.message===",JSON.stringify(event.message));
-                console.log("===3.event.message.text===",JSON.stringify(event.message.text));
-                if(event.message && event.message.text){
-                    lib.decideMessage(sender, event.message.text, false);
-                }
-
-                if(event.postback){
-                    let text = JSON.stringify(event.postback.payload);
-                    lib.decideMessage(sender, text, true);
-                }
-            })
-        //});
-    }*/
-    /*
-    let data = req.body;
-    let messaging_events = data.entry[0].messaging;
- 
-    for(let i=0; i< messaging_events.length; i++){
-        let event = messaging_events[i];
-        let sender = event.sender.id;
-
-        if(event.message && event.message.text){
-            lib.decideMessage(sender, event.message.text, false);
-        }
-
-        if(event.postback){
-            let text = JSON.stringify(event.postback.payload);
-            lib.decideMessage(sender, text, true);
-        }
-        
-    }
-    */
     res.sendStatus(200);
 
 });
@@ -108,3 +63,95 @@ event.message
 app.listen(app.get('port'), ()=>{
     console.log("running: port", app.get('port'));
 });
+
+
+/**funcs
+ * 
+ */
+
+function decideMessage(sender, text, isPostback=false){
+    selfSender = sender;
+    text = text.toLowerCase();
+    if(!isPostback){
+        if(text.includes("offer"))
+        {
+            let triggerPayload='0';
+            sendText(sender, "Here are offers: ");
+            
+            getSelectionByTriggerPayload2temp(triggerPayload);
+            if(selfTemp == null){
+                sendText(sender, "No more buttons in collection");
+            }else{
+                sendButtonMessage(sender, "Choose one of those:",selfTemp);
+            }
+        }else if(text.includes('debug')){
+            sendText(sender, "debug4");
+        } else {
+            sendText(sender, "Sorry, I don't understand that");
+        }
+    }else{
+        let triggerPayload = text.toLowerCase();
+        sendText(sender, "From your selection(pload="+triggerPayload+"), we have:");
+        
+        getSelectionByTriggerPayload2temp(triggerPayload);
+        if(selfTemp == null){
+            sendText(sender, "No more buttons in collection");
+        }else{
+            sendButtonMessage(sender, "Choose one of those:",selfTemp);
+        }
+    }
+}
+
+function getSelectionByTriggerPayload2temp(triggerPayload){
+    selfTemp = null;
+    sendText(selfSender, "total:"+vars.collections.length);
+    for( let i=0; i<vars.collections.length; i++){
+        let c = vars.collections[i];
+
+        sendText(selfSender, "if "+c.triggerPayload+"="+triggerPayload+"?");
+        if(c.triggerPayload.toLowerCase() == triggerPayload.toLowerCase()){
+            selfTemp = c.data;
+            sendText(selfSender, "Yes=");
+            break;
+        }else{
+            sendText(selfSender, "thisNot=");
+        }
+    }
+}
+
+function sendButtonMessage (sender, text, buttons){
+    let messageData = {
+        "attachment":{
+            "type":"template",
+            "payload":{
+                "template_type":"button",
+                "text":text,
+                "buttons":buttons
+            }
+        }
+    }
+    sendRequest(sender, messageData);
+}
+
+function sendRequest(sender, messageData){
+        request({
+        url:secret.requestUri,
+        qs:{access_token : secret.access_token},
+        method:"POST",
+        json: {
+            recipient: { id: sender},
+            message: messageData
+        }
+    }, (error, response, body)=>{
+        if(error){
+            console.log("sending error");
+        }else if(response.body.error){
+            console.log("response body error");
+        }
+    });
+}
+
+function sendText(sender, text){
+    let messageData = {text: text};
+    sendRequest(sender, messageData);
+}
