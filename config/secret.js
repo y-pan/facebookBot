62a9263167d100b2219ddb0aa80c13926daba628
Yun Kui Pan <panyunkui@gmail.com>
2018-01-28 18:40:51 -0500

    remove token, for security concerns

--- config/secret.js
+++ config/secret.js
@@ -1,6 +1,6 @@
 module.exports = {
-    verify_token:'DontTellOthers'
-    ,access_token:'EAAbvwR6d39IBALEjKwWKktsmCnMySYE2T2xJUUNU2gVxItuJoYLwNQbCQKIhwF4ZAeg7BsJYSUAtBXXxQ1VwtdAPDetvyC6WrCxtLb6simlSiWWIK6WfgteqVnihwssDQRFSpPz9NzdOst6twbsFS5gvtcmhsz8fOhb1f8AZDZD'
+    verify_token:''
+    ,access_token:''
     ,requestUri:'https://graph.facebook.com/v2.6/me/messages'
     ,db_cloud:'_TO_BE_ADDED_'
     ,db_local: 'mongodb://localhost:27017/mybot'
