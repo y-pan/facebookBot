1c95aff72346740ab4dedaf97592131c574d9238
Yun Kui Pan <panyunkui@gmail.com>
2018-01-29 18:10:51 -0500

    test

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const mongoose = require('mongoose');
+const Camera = require('./models/camera');
 
 // const secret = require('./config/secret');          // secret vars
 const vars = require('./config/vars');              // vars (general config vars)
@@ -35,6 +36,12 @@ if(vars.useLocal){
 mongoose.Promise = global.Promise;  // db will always use local
 mongoose.connect(dbConnection).then(()=>{
     dbConnectionStatus += "SUCCESSFUL";
+    Camera.findAll().then((data)=>{
+        dbConnectionStatus += " fetched data: " + data + " " + data.length;
+    }).catch((data)=>{
+        dbConnectionStatus += " failed to validate data somehow~~~, try later";
+
+    });
 }).catch(()=>{
     dbConnectionStatus += "FAILED";
 });
@@ -84,7 +91,7 @@ app.post('/webhook', function (req, res) {
              * ******************************************
              * ******************************************
             */
-        } else if(event.postback) {
+        } else if(event.postback) { // not my concern so far
             replyMessageOrPostback(event,null);   // user postback to bot
         }else{                  
           /******************************** to add more events ******************************/
@@ -107,9 +114,9 @@ app.listen(app.get('port'), ()=>{
 
 
 /**
- * 
- *  Here are functions of logic
- * 
+ * ---------------------------------------------------------------------------
+ *                    Here are functions of logic
+ * ---------------------------------------------------------------------------
  */
 
 
@@ -258,107 +265,3 @@ function sendGenericMessage(recipientId, elements) {
   callSendAPI(messageData);
 }
 
-
-/*
-function retrieveTeById(id, callback){
-    let text;
-    for(let i=0; i< db.te.length; i++){
-        if(db.te[i].id == id) {text=db.te[i].data; break;}
-    }
-    callback(text);
-}
-*/
-/*
-function sendButtonMessageOnPostback(recipientId, messageText) {
-    var messageData = {   
-        recipient: { id: recipientId },
-        message: {
-            "attachment":{
-                "type":"template",
-                "payload":{
-                    "template_type":"button",
-                    "text":messageText,
-                    "buttons":[
-                        { type:"postback", title:"1->menu1", payload:"te4" },
-                        { type:"postback", title:"1->menu2", payload:"te5" },
-                        { type:"postback", title:"1->menu3", payload:"te6" }
-                    ]
-                }
-            }
-        }
-    }
-  callSendAPI(messageData);
-}
-*/
-
-/*
-function reply_receivedMessage(event, dataString){
-    let tb = dataString.substring(0,2);
-    let ids = dataString.substring(2);
-    
-    switch(tb){
-        case "te":  // assume "te" (text type) always be single
-            lib.retrieveTeById(ids,(text)=>{
-                sendTextMessage(event.sender.id, text);
-            })
-            break;
-        case "bu":  // bu can be multiple: "bu1,2,3"
-            //sendTextMessage(event.sender.id, "Oops, still under developing in receivedPostback() for other tb type");
-            lib.retrieveBuById(ids,(buttons)=>{
-                if(buttons.length <= 0){
-                    sendTextMessage(event.sender.id,"Oops, can't find any buttons~~~");
-                }else{
-                    sendButtonMessage(event.sender.id,"Please select one:",buttons);
-                }
-            });
-            
-            
-            break;
-        default:
-            sendTextMessage(event.sender.id, "Oops, still under developing in reply_receivedMessage() for other tb type");
-            break;
-    }
-}
-
-function receivedPostback(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfPostback = event.timestamp;
-  var payload = event.postback.payload;
-  console.log("Received postback for user %d and page %d with payload '%s' " + "at %d", senderID, recipientID, payload, timeOfPostback);
-
-  let tb = payload.substring(0, 2); // db collection(table) name
-  let ids = payload.substring(2);    // id in the collection
-  switch(tb){
-    case "te":
-        lib.retrieveTeById(ids,(text)=>{
-            sendTextMessage(senderID, text);
-        })
-        break;
-    case "bu":
-        lib.retrieveBuById(ids,(buttons)=>{
-            if(buttons.length <= 0){
-                sendTextMessage(event.sender.id,"Oops, can't find any buttons~~~");
-            }else{
-                sendButtonMessage(event.sender.id,"Please select one:",buttons);
-            }
-        });
-        break;
-    case "im":
-        sendTextMessage(event.sender.id, "Oops, still under developing in receivedPostback() for other tb type");
-        break;
-    case "ge":
-        lib.retrieveGeById(ids,(elemetns) => {
-            if(elements.length <= 0){
-                sendTextMessage(event.sender.id,"Oops, can't find any generic record~~~");
-            }else{
-                sendGenericMessage(event.sender.id,elements);
-            }
-        });
-        break;
-    default:
-        sendTextMessage(event.sender.id, "Oops, still under developing in receivedPostback() for other tb type");
-        break;
-  }
-}
-*/
