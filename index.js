adaa393dfe023e93ab5aa51c5d2f3b324c050250
Yun Kui Pan <panyunkui@gmail.com>
2018-01-30 00:07:08 -0500

    test

--- index.js
+++ index.js
@@ -20,14 +20,14 @@ let dbConnectionStatus = "Connection to";
 
 let useLocal = vars.useLocal;
 // useLocal = true;
-if(useLocal){
+if (useLocal) {
     secret = require('./config/secret');
     verify_token = secret.access_token;
     access_token = secret.access_token;
     testingVar = "_local_testing_var_";
     dbConnection = secret.db_local;
     dbConnectionStatus += " LOCAL-DB: ";
-}else{ // not onlly db, but env variables are from heroku env
+} else { // not onlly db, but env variables are from heroku env
     verify_token = process.env.verify_token;
     access_token = process.env.access_token;
     testingVar = process.env.testingVar;
@@ -36,14 +36,14 @@ if(useLocal){
 }
 
 mongoose.Promise = global.Promise;  // db will always use local
-mongoose.connect(dbConnection).then(()=>{
+mongoose.connect(dbConnection).then(() => {
     dbConnectionStatus += "SUCCESSFUL";
-    Camera.findAll_pm().then((data)=>{
+    Camera.findAll_pm().then((data) => {
         dbConnectionStatus += " fetched 1st record: " + data[0] + " TOTAL data size:" + data.length;
-    }).catch((data)=>{
+    }).catch((data) => {
         dbConnectionStatus += " failed to validate data somehow~~~, check server/db, try later";
     });
-}).catch(()=>{
+}).catch(() => {
     dbConnectionStatus += "FAILED";
 });
 
@@ -51,18 +51,18 @@ app.set('port', (process.env.PORT || 5000));
 
 // Allow us to process the data
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // routes
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     let greeting = 'Hi I am a facebookbot, v1: testingVar= ' + testingVar + "\n" + dbConnectionStatus;
     console.log(greeting)
     res.send(greeting);
 });
 
 // Facebook
-app.get('/webhook', function(req, res){
-    if(req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === verify_token){
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === verify_token) {
         console.log("Validating webhook");
         res.status(200).send(req.query['hub.challenge']); // good
     }
@@ -71,45 +71,45 @@ app.get('/webhook', function(req, res){
 });
 
 app.post('/webhook', function (req, res) {
-  var data = req.body;
+    var data = req.body;
 
-  // Make sure this is a page subscription
-  if (data.object === 'page') {
+    // Make sure this is a page subscription
+    if (data.object === 'page') {
 
-    // Iterate over each entry - there may be multiple if batched
-    data.entry.forEach(function(entry) {
-      var pageID = entry.id;
-      var timeOfEvent = entry.time;
+        // Iterate over each entry - there may be multiple if batched
+        data.entry.forEach(function (entry) {
+            var pageID = entry.id;
+            var timeOfEvent = entry.time;
 
-      // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-        if (event.message) {
-            receivedMessage(event);       // user send text into bot 
-            /**
-             * ******************************************
-             * ******************************************
-             *             [main entryPoint]  
-             * ******************************************
-             * ******************************************
-            */
-        } else if(event.postback) { // not my concern so far
-            replyMessageOrPostback(event,null);   // user postback to bot
-        }else{                  
-          /******************************** to add more events ******************************/
-            console.log("Webhook received unknown event: ", event);
-        }
-      });
-    });
+            // Iterate over each messaging event
+            entry.messaging.forEach(function (event) {
+                if (event.message) {
+                    receivedMessage(event);       // user send text into bot 
+                    /**
+                     * ******************************************
+                     * ******************************************
+                     *             [main entryPoint]  
+                     * ******************************************
+                     * ******************************************
+                    */
+                } else if (event.postback) { // not my concern so far
+                    replyMessageOrPostback(event, null);   // user postback to bot
+                } else {
+                    /******************************** to add more events ******************************/
+                    console.log("Webhook received unknown event");//, event);
+                }
+            });
+        });
 
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds. Otherwise, the request
-    // will time out and we will keep trying to resend.
-    res.sendStatus(200);
-  }
+        // Assume all went well.
+        //
+        // You must send back a 200, within 20 seconds. Otherwise, the request
+        // will time out and we will keep trying to resend.
+        res.sendStatus(200);
+    }
 });
 
-app.listen(app.get('port'), ()=>{
+app.listen(app.get('port'), () => {
     console.log("running: port", app.get('port'));
 });
 
@@ -122,153 +122,157 @@ app.listen(app.get('port'), ()=>{
 
 
 function receivedMessage(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfMessage = event.timestamp;
-  var message = event.message;
-  console.log("Received message for user %d and page %d at %d with message:", senderID, recipientID, timeOfMessage);
-  console.log(JSON.stringify(message));
-  var messageId = message.mid;
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
+    console.log("Received message for user %d and page %d at %d with message:", senderID, recipientID, timeOfMessage);
+    console.log(JSON.stringify(message));
+    var messageId = message.mid;
 
-  // You may get a text or attachment but not both
-  var messageText = message.text.toLowerCase();
-  var messageAttachments = message.attachments;
+    // You may get a text or attachment but not both
+    var messageText = message.text.toLowerCase();
+    var messageAttachments = message.attachments;
 
-  if (messageText) {
-    // text
-    
-    searchDb_pm(messageText)
-        .then(urls =>{
-            // let str = "";
-            // urls = ["http://opendata.toronto.ca/transportation/tmc/rescucameraimages/CameraImages/loc9113.jpg",
-            //  "https://www.toronto.ca/data/transportation/roadrestrictions/CameraImages/loc8073.jpg"]
-            // urls.forEach(u => str+= u + ",")
-            // if(str.length > 1){
-            //     str = str.substring(0, str.length -1);
-            // }
-            // sendTextMessage(senderID, str);
-            urls.forEach(url =>{
-                sendTextMessage(senderID, url);
+    if (messageText) {
+        // text
+
+        searchDb_pm(messageText)
+            .then(urls => {
+                // let str = "";
+                // urls = ["http://opendata.toronto.ca/transportation/tmc/rescucameraimages/CameraImages/loc9113.jpg",
+                //  "https://www.toronto.ca/data/transportation/roadrestrictions/CameraImages/loc8073.jpg"]
+                // urls.forEach(u => str+= u + ",")
+                // if(str.length > 1){
+                //     str = str.substring(0, str.length -1);
+                // }
+                // sendTextMessage(senderID, str);
+                console.log("@@@url size: " + urls.length);
+                urls.forEach(url => {
+                    sendTextMessage(senderID, url);
+                });
+
+                // replyMessageOrPostback(event, str);
+            })
+            .catch(err => {
+                sendTextMessage(senderID, err);
             });
-            
-            // replyMessageOrPostback(event, str);
-        })
-        .catch(err=>{
-            sendTextMessage(senderID, err);
-        });
 
-  } else if (messageAttachments) {      /******************** to add attachemnt message (non-text type) **********************/
-    sendTextMessage(senderID, "Message with attachment received, need to implement to store it in db (url) and server (actual file)");
-  }
+    } else if (messageAttachments) {      /******************** to add attachemnt message (non-text type) **********************/
+        sendTextMessage(senderID, "Message with attachment received, need to implement to store it in db (url) and server (actual file)");
+    }
 }
 
 
 
 
 function sendTextMessage(recipientId, messageText) {
-  var messageData = {   recipient: { id: recipientId },
-                        message: { text: messageText }
-  };
-  callSendAPI(messageData);
+    var messageData = {
+        recipient: { id: recipientId },
+        message: { text: messageText }
+    };
+    callSendAPI(messageData);
 }
 
 
 
 function sendButtonMessage(recipientId, messageText, buttons) {
-    var messageData = {   
+    var messageData = {
         recipient: { id: recipientId },
         message: {
-            "attachment":{
-                "type":"template",
-                "payload":{
-                    "template_type":"button",
-                    "text":messageText,
-                    "buttons":buttons
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "button",
+                    "text": messageText,
+                    "buttons": buttons
                 }
             }
         }
     }
-  callSendAPI(messageData);
+    callSendAPI(messageData);
 }
 
 // if payload == null, then it is replying the incomming message, not postback
-function replyMessageOrPostback(event, payload=null){
+function replyMessageOrPostback(event, payload = null) {
     let tb, ids;
-    if(!payload){
+    if (!payload) {
         console.log("Received postback for user %d and page %d with payload '%s' " + "at %d", event.sender.id, event.recipient.id, event.postback.payload, event.timestamp);
         payload = event.postback.payload;
     }
-    console.log("YUN => PAYLOAD: " + payload);
+    console.log("@@@PAYLOAD: " + payload);
     sendTextMessage(event.sender.id, "REPLYING MSG: " + payload);
 
 }
 
 function callSendAPI(messageData) {
-    console.log("@@@begin callSendAPI" );
-  request({
-    uri: vars.requestUri,
-    qs: { access_token: access_token },
-    method: 'POST',
-    json: messageData
+    console.log("@@@begin callSendAPI");
+    request({
+        uri: vars.requestUri,
+        qs: { access_token: access_token },
+        method: 'POST',
+        json: messageData
 
-  }, function (error, response, body) {
-    console.log("@@@begin callSendAPI => messageData\n");
-    console.log(messageData );
+    }, function (error, response, body) {
+        console.log("@@@begin callSendAPI => messageData\n");
+        console.log(messageData);
 
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-      console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      console.error("@@@begin callSendAPI => response:n");
-      console.error(response);
-      console.error("@@@begin callSendAPI => error: \n");
-      console.error(error);
-    }
-  });  
+        if (!error && response.statusCode == 200) {
+            var recipientId = body.recipient_id;
+            var messageId = body.message_id;
+            console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
+        } else {
+            console.error("Unable to send message.");
+            console.error("@@@begin callSendAPI => response:n");
+            console.error(response);
+            console.error("@@@begin callSendAPI => error: \n");
+            console.error(error);
+        }
+    });
 }
 /** not in use | to be changed*/
 function sendGenericMessage(recipientId, elements) {
-   // need function with callback to return generic elemetns (how to link different type of element together? like genericId? )
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "generic",
-          elements: elements
+    // need function with callback to return generic elemetns (how to link different type of element together? like genericId? )
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            attachment: {
+                type: "template",
+                payload: {
+                    template_type: "generic",
+                    elements: elements
+                }
+            }
         }
-      }
-    }
-  };  
-  callSendAPI(messageData);
+    };
+    callSendAPI(messageData);
 }
 
 
 
-function searchDb_pm (text) {
-    return new Promise((res, rej) =>{
+function searchDb_pm(text) {
+    return new Promise((res, rej) => {
         let feedback = null;
         let tags = text.split(" ");
         // logic to determing which collection to search, now only look into "camera" in db
-        console.log("@@@TAGS:"+text);
+        console.log("@@@TAGS:" + text);
 
         // urls = ["http://opendata.toronto.ca/transportation/tmc/rescucameraimages/CameraImages/loc9113.jpg",
         //      "https://www.toronto.ca/data/transportation/roadrestrictions/CameraImages/loc8073.jpg"]
         // res(urls);
 
-        Camera.findAll_pm().then((urls)=>{
-            if(urls.constructor === Array){
-                res(urls);
-            }
-            rej("some error occursed");
-        }).catch((err)=>{
-            rej("some error occursed");
-        });
+        Camera.findAll_pm()
+            .then((urls) => {
+                if (urls.constructor === Array) {
+                    res(urls[0]);
+                } else {
+                    rej("some error occursed");
+                }
+            }).catch((err) => {
+                rej("some error occursed");
+            });
 
         // Camera.findAllByQueryObject_pm({"tags": tags}).then(urls =>{
         //     res(urls);
