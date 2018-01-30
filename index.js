cc5f73ee482af1e79d2b3ab63c431bcfeac8ee70
Yun Kui Pan <panyunkui@gmail.com>
2018-01-29 20:38:01 -0500

    test

--- index.js
+++ index.js
@@ -140,8 +140,7 @@ function receivedMessage(event) {
     lib.searchDb_pm(messageText)
         .then(urls =>{
             let str = "";
-            urls = ["http://opendata.toronto.ca/transportation/tmc/rescucameraimages/CameraImages/loc9113.jpg",
-             "https://www.toronto.ca/data/transportation/roadrestrictions/CameraImages/loc8073.jpg"]
+            urls = ["http://opendata.toronto.ca/transportation/tmc/rescucameraimages/CameraImages/loc9113.jpg"]
             urls.forEach(u => str+= u + ",")
             if(str.length > 1){
                 str = str.substring(0, str.length -1);
